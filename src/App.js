0a90f83c6acb51d1c91b53e2865f1e9d12fd626e
mwyywm <hasanof.rasmus@gmail.com>
2021-08-02 20:29:36 +0200

    Generates random colors on start, added button to generate random colors and made changes to copy to clipboard state.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./App.css";
 
+function random_hex_color() {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return "#" + n.slice(0, 6);
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      input1: "#d83131",
-      input2: "#fff56b",
-      tooltext: "Copy to clipboard",
+      input1: random_hex_color(),
+      input2: random_hex_color(),
+      tooltext: "Copy to clipboard!",
     };
   }
   handleChange = this.handleChange.bind(this);
   handleSubmit = this.handleSubmit.bind(this);
   copyToClipboard = this.copyToClipboard.bind(this);
+  generateColors = this.generateColors.bind(this);
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -22,14 +28,18 @@ class App extends React.Component {
   handleSubmit(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
-
   copyToClipboard(e) {
     if (e.target.lastChild.innerText === undefined) {
       console.log("this will happen when its undefined");
-      this.setState({ tooltext: "Copied" });
       e = navigator.clipboard.writeText(e.target.innerText).then(
         () => {
           // on success
+          this.setState({ tooltext: "Copied!" });
+          // After 3 seconds I want the tooltext to go back to "Copy to clipboard!".
+          setTimeout(
+            () => this.setState({ tooltext: "Copy to clipboard!" }),
+            3000
+          );
           console.log(`successfully copied to clipboard`);
         },
         () => {
@@ -41,7 +51,12 @@ class App extends React.Component {
       e = navigator.clipboard.writeText(e.target.lastChild.innerText).then(
         () => {
           // on success
-          this.setState({ tooltext: "Copied" });
+          this.setState({ tooltext: "Copied!" });
+          // After 3 seconds I want the tooltext to go back to "Copy to clipboard!".
+          setTimeout(
+            () => this.setState({ tooltext: "Copy to clipboard!" }),
+            3000
+          );
           console.log(`successfully copied to clipboard`);
         },
         () => {
@@ -54,6 +69,13 @@ class App extends React.Component {
     // console.log("lastchild", e.target.lastChild.innerText.startsWith("linear"));
   }
 
+  generateColors() {
+    this.setState({
+      input1: random_hex_color(),
+      input2: random_hex_color(),
+      tooltext: "Copy to clipboard!",
+    });
+  }
   render() {
     return (
       <div
@@ -63,8 +85,11 @@ class App extends React.Component {
         }}
       >
         <header className="App-header">
-          <h1>This is a CSS gradient generator</h1>
-          <p>Pick two colors</p>
+          <h1>CSS gradient generator</h1>
+          <p>Pick two colors and then copy to clipboard</p>
+          <button onClick={this.generateColors} className="btn">
+            Click here to generate random colors!
+          </button>
           <div className="colorpickers">
             <input
               value={this.state.input1}
